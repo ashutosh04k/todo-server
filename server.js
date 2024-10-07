b7556a86382ca02789393225b6f96047bcae0ca4
ashutosh04k <kumarasutosh2014@gmail.com>
2024-10-07 13:09:45 +0530

    cors error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const TodoModel = require("./models/todoList")
 
 var app = express();
-app.use(cors());
+app.use(cors({ origin: 'http://13.233.160.191:3000' }));
 app.use(express.json());
 
 // Connect to your MongoDB database (replace with your database URL)
